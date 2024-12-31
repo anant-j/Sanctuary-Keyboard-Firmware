6c4b343c47205685e40ddeba87795dad040df5ab
Anant Jain <anant.j2409@gmail.com>
2024-12-30 21:15:15 -0800

    Cleanup

--- KeyboardFirmwareRev1/KeyboardFirmwareRev1.ino
+++ KeyboardFirmwareRev1/KeyboardFirmwareRev1.ino
@@ -4,3 +4,24 @@
 
 BleKeyboard Kbd("Sanctuary", "LegoRocket");
 Adafruit_NeoPixel pixels(NUM_PIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+// Function to set NeoPixel color
+void setNeoPixelColor(int red, int green, int blue) {
+  pixels.setPixelColor(0, pixels.Color(red, green, blue));
+  pixels.show();
+}
+
+// Method to enable NeoPixel power
+void enableNeoPixelPower() {
+  pinMode(NEOPIXEL_I2C_POWER, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(NEOPIXEL_I2C_POWER, HIGH); // Pull power pin high to enable NeoPixel
+}
+
+// Method to initialize NeoPixel
+void initializeNeoPixel() {
+  pixels.begin();
+  pixels.clear();
+  pixels.setBrightness(10); // Adjust brightness (0-255)
+  pixels.show();
+}
--- KeyboardFirmwareRev1/_Main.ino
+++ KeyboardFirmwareRev1/_Main.ino
@@ -6,17 +6,24 @@ void setup() {
   }
 
   // Enable NeoPixel power
-  pinMode(NEOPIXEL_I2C_POWER, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(NEOPIXEL_I2C_POWER, HIGH); // Pull power pin high to enable NeoPixel
+  enableNeoPixelPower();
 
   // Initialize NeoPixel
-  pixels.begin();
-  pixels.clear();
-  pixels.setBrightness(10); // Adjust brightness (0-255)
-  pixels.show();
+  initializeNeoPixel();
 
-  // Initialize row output pins and set default state
+  // Initialize row and column pins
+  initializeRowPins();
+  initializeColPins();
+
+  // Start Bluetooth keyboard
+  startBluetoothKeyboard();
+
+  // Set NeoPixel to red (searching for BT device)
+  setNeoPixelColor(255, 0, 0);
+}
+
+// Method to initialize row output pins
+void initializeRowPins() {
   for(int i = 0; i < NumRows; i++) {
     pinMode(Rows[i], OUTPUT);  
     digitalWrite(Rows[i], LOW);
@@ -28,8 +35,10 @@ void setup() {
   if(DEBUG) {
     Serial.println("row output pin complete...");
   }
+}
 
-  // Initialize column pins as input, with Pulldown
+// Method to initialize column input pins
+void initializeColPins() {
   for(int i = 0; i < NumCols; i++) {
     pinMode(Cols[i], INPUT_PULLDOWN);
     if(DEBUG) {
@@ -40,8 +49,10 @@ void setup() {
   if(DEBUG) {
     Serial.println("Col output pin complete...");
   }
-  
-  // Start Bluetooth keyboard
+}
+
+// Method to start Bluetooth keyboard
+void startBluetoothKeyboard() {
   if(DEBUG) {
     Serial.println("kbd begin...");
   }
@@ -49,108 +60,19 @@ void setup() {
   if(DEBUG) {
     Serial.println("kbd begin complete...");
   }
-
-  // Set NeoPixel to red (searching for BT device)
-  setNeoPixelColor(255, 0, 0);
 }
 
-// Trackers for row and layer
 int RowCnt = 0; 
 int LayerCnt = 0;
-
-// Loops to iterate through all functions
+// Method to handle the loop functionality
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH); 
-  // Check if the keyboard is connected
+
   if(Kbd.isConnected()) {
     // Set NeoPixel to blue (connected)
     setNeoPixelColor(0, 0, 255);
-
     // Scan the matrix
-    digitalWrite(Rows[RowCnt], HIGH);
-    int ColCnt = 0;
-
-    // Check each column
-    while(ColCnt <= (NumCols - 1)) {
-      // Check key press
-      if(digitalRead(Cols[ColCnt]) == HIGH && PressedCheck[LayerCnt][RowCnt][ColCnt] == OFF) {
-        // Process key press
-        switch(Layer1[LayerCnt][RowCnt][ColCnt]) {
-          case 0:
-          case 1:
-          case 2:
-            // changeID(Layer1[LayerCnt][RowCnt][ColCnt]);
-            break;
-          case 3:
-            Kbd.press(KEY_MEDIA_PLAY_PAUSE);
-            break;
-          case 5:
-            ESP.restart();
-            break;
-          case FUNCTION_SW:
-            Kbd.releaseAll();
-            LayerCnt++;
-            break;
-          case NULL_CON:
-            break;
-          case NEXT:
-            Kbd.press(KEY_MEDIA_NEXT_TRACK);
-            Kbd.releaseAll();
-            break;
-          case PREV:
-            Kbd.press(KEY_MEDIA_PREVIOUS_TRACK);
-            break;
-          default:
-            Kbd.press(Layer1[LayerCnt][RowCnt][ColCnt]);
-        }
-        PressedCheck[LayerCnt][RowCnt][ColCnt] = ON;   
-      } 
-      // Check key release
-      else if(digitalRead(Cols[ColCnt]) == LOW && PressedCheck[LayerCnt][RowCnt][ColCnt] == ON) {
-        // Reset NeoPixel to blue (connected)
-        setNeoPixelColor(0, 0, 255);
-
-        // Process key release
-        switch(Layer1[LayerCnt][RowCnt][ColCnt]) {
-          case 0:
-          case 1:
-          case 2:
-            break;
-          case 3:
-            Kbd.release(KEY_MEDIA_PLAY_PAUSE);
-            break;
-          case FUNCTION_SW:
-            PressedCheck[LayerCnt][RowCnt][ColCnt] = OFF;
-            if(LayerCnt > 0) {
-              LayerCnt--;
-            }
-            Kbd.releaseAll();
-            break;
-          case NULL_CON:
-            PressedCheck[LayerCnt][RowCnt][ColCnt] = OFF;
-            if(LayerCnt > 0) {
-              LayerCnt--;
-            }
-            Kbd.releaseAll();
-            break;
-          case NEXT:
-            Kbd.release(KEY_MEDIA_NEXT_TRACK);
-            break;
-          case PREV:
-            Kbd.release(KEY_MEDIA_PREVIOUS_TRACK);
-            break;
-          default:
-            Kbd.release(Layer1[LayerCnt][RowCnt][ColCnt]);
-        }
-        PressedCheck[LayerCnt][RowCnt][ColCnt] = OFF;
-      }
-      ColCnt++;
-    }
-    digitalWrite(Rows[RowCnt], LOW);
-    RowCnt++;
-    if(RowCnt >= (NumRows)) {
-      RowCnt = 0;
-    }
+    scanMatrix();
   } else {
     // Set NeoPixel to red (searching for BT device)
     setNeoPixelColor(255, 0, 0);
@@ -159,8 +81,95 @@ void loop() {
   delay(1);
 }
 
-// Function to set NeoPixel color
-void setNeoPixelColor(int red, int green, int blue) {
-  pixels.setPixelColor(0, pixels.Color(red, green, blue));
-  pixels.show();
+// Method to scan the matrix and handle key presses
+void scanMatrix() {
+  digitalWrite(Rows[RowCnt], HIGH);
+  int ColCnt = 0;
+
+  // Check each column
+  while(ColCnt <= (NumCols - 1)) {
+    // Handle key press
+    if(digitalRead(Cols[ColCnt]) == HIGH && PressedCheck[LayerCnt][RowCnt][ColCnt] == OFF) {
+      handleKeyPress(ColCnt);
+    } 
+    // Handle key release
+    else if(digitalRead(Cols[ColCnt]) == LOW && PressedCheck[LayerCnt][RowCnt][ColCnt] == ON) {
+      handleKeyRelease(ColCnt);
+    }
+    ColCnt++;
+  }
+  digitalWrite(Rows[RowCnt], LOW);
+  RowCnt++;
+  if(RowCnt >= (NumRows)) {
+    RowCnt = 0;
+  }
+}
+
+// Method to handle key press events
+void handleKeyPress(int ColCnt) {
+  switch(Layer1[LayerCnt][RowCnt][ColCnt]) {
+    case 0:
+    case 1:
+    case 2:
+      // changeID(Layer1[LayerCnt][RowCnt][ColCnt]);
+      break;
+    case 3:
+      Kbd.press(KEY_MEDIA_PLAY_PAUSE);
+      break;
+    case 5:
+      ESP.restart();
+      break;
+    case FUNCTION_SW:
+      Kbd.releaseAll();
+      LayerCnt++;
+      break;
+    case NULL_CON:
+      break;
+    case NEXT:
+      Kbd.press(KEY_MEDIA_NEXT_TRACK);
+      Kbd.releaseAll();
+      break;
+    case PREV:
+      Kbd.press(KEY_MEDIA_PREVIOUS_TRACK);
+      break;
+    default:
+      Kbd.press(Layer1[LayerCnt][RowCnt][ColCnt]);
+  }
+  PressedCheck[LayerCnt][RowCnt][ColCnt] = ON;   
+}
+
+// Method to handle key release events
+void handleKeyRelease(int ColCnt) {
+  switch(Layer1[LayerCnt][RowCnt][ColCnt]) {
+    case 0:
+    case 1:
+    case 2:
+      break;
+    case 3:
+      Kbd.release(KEY_MEDIA_PLAY_PAUSE);
+      break;
+    case FUNCTION_SW:
+      PressedCheck[LayerCnt][RowCnt][ColCnt] = OFF;
+      if(LayerCnt > 0) {
+        LayerCnt--;
+      }
+      Kbd.releaseAll();
+      break;
+    case NULL_CON:
+      PressedCheck[LayerCnt][RowCnt][ColCnt] = OFF;
+      if(LayerCnt > 0) {
+        LayerCnt--;
+      }
+      Kbd.releaseAll();
+      break;
+    case NEXT:
+      Kbd.release(KEY_MEDIA_NEXT_TRACK);
+      break;
+    case PREV:
+      Kbd.release(KEY_MEDIA_PREVIOUS_TRACK);
+      break;
+    default:
+      Kbd.release(Layer1[LayerCnt][RowCnt][ColCnt]);
+  }
+  PressedCheck[LayerCnt][RowCnt][ColCnt] = OFF;
 }
