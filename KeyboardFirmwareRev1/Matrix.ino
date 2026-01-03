720f28438f503b81887b9e79a8ec03234f75e4dc
Anant Jain <anant.j2409@gmail.com>
2026-01-02 17:10:58 -0800

    Working copy 1

--- KeyboardFirmwareRev1/Matrix.ino
+++ KeyboardFirmwareRev1/Matrix.ino
@@ -1,24 +1,25 @@
 // Rows to scan, and Columns to read (definitions in SanctuaryHardware
-short Rows[NumRows] = {Row1, Row2, Row3, Row4, Row5};
-short Cols[NumCols] = {Col1, Col2, Col3, Col4, Col5, Col6, Col7, Col8, Col9, Col10, Col11, Col12};
+short Rows[NumRows] = {Row1, Row2, Row3, Row4, Row5, Row6};
+short Cols[NumCols] = {Col1, Col2, Col3, Col4, Col5, Col6, Col7, Col8, Col9, Col10, Col11, Col12, Col13, Col14, Col15, Col16, Col17, Col18};
 
 //  Layer 1 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int AllLayers[NumLayers][NumRows][NumCols] =
     {
         {
-            {FUNC_LAYER, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '['},
-            {FUNC_LAYER, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '['},
-            {FUNC_LAYER, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '['},
-            {FUNC_LAYER, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '['},
-            {FUNC_LAYER, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '['},
-        },
-        {
-            {FUNC_LAYER, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'z'},
-            {FUNC_LAYER, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'z'},
-            {FUNC_LAYER, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'z'},
-            {FUNC_LAYER, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'z'},
-            {FUNC_LAYER, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'z'},
-        },
+            {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
+            {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
+            {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
+            {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
+            {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
+            {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},                        
+        }
+        // {
+        //     {FUNC_LAYER, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'z'},
+        //     {FUNC_LAYER, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'z'},
+        //     {FUNC_LAYER, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'z'},
+        //     {FUNC_LAYER, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'z'},
+        //     {FUNC_LAYER, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 'z'},
+        // },
 };
 
 // define a map to store different colors/rgb for different layers
@@ -26,7 +27,7 @@ int AllLayers[NumLayers][NumRows][NumCols] =
 int BluetoothDisconnectColor[4] = {255, 0, 0, 10}; // Red
 int LayerColors[NumLayers][4] = {
     {0, 255, 0, 5}, // Green
-    {0, 0, 255, 5}, // Blue
+    // {0, 0, 255, 5}, // Blue
 };
 
 // Array to check if a key is currently pressed - Initiate everything to 0
--- KeyboardFirmwareRev1/SanctuaryHardware.h
+++ KeyboardFirmwareRev1/SanctuaryHardware.h
@@ -7,28 +7,36 @@
 
 // Pin Definitions
 #define Row1 4
-#define Row2 5
+#define Row2 12
 #define Row3 6
 #define Row4 7
 #define Row5 8
+#define Row6 3
 
-#define Col1 3
-#define Col2 46
-#define Col3 9
-#define Col4 10
-#define Col5 1
-#define Col6 2
-#define Col7 42
-#define Col8 41
-#define Col9 40
-#define Col10 39
-#define Col11 38
-#define Col12 37
+#define Col1 46
+#define Col2 9
+#define Col3 10
+#define Col4 1
+#define Col5 2
+#define Col6 42
+#define Col7 41
+#define Col8 40
+#define Col9 39
+#define Col10 38
+#define Col11 37
+#define Col12 36
+#define Col13 35
+#define Col14 15
+#define Col15 16
+#define Col16 17
+#define Col17 18
+#define Col18 11
+
 
 // Matrix setup
-#define NumRows 5
-#define NumCols 12
-#define NumLayers 2
+#define NumRows 6
+#define NumCols 18
+#define NumLayers 1
 
 // ANY CUSTOM VALUES MUST BE > 255
 #define FUNC_LAYER 256
